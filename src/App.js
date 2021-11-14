b713a4d80f23f769da70f13b72e4df4d5c402bfc
rizzie03 <destin.rizzie@gmail.com>
2021-11-14 12:51:17 +0700

    adding details and comment section and second page for the logged on user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignUp from "./pages/signup/SignUp";
 import SignIn from "./pages/signin/SignIn";
+import Comments from "./pages/comments/Comments"
+import CommentsUser from "./pages/comments/CommentsUser"
 
 import Headers from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/signin" element={<SignIn />} />
+        <Route exact path="/comments" element={<Comments />} />
+        <Route exact path="/comments/user" element={<CommentsUser />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/comments/CommentsUser.jsx
@@ -0,0 +1,10 @@
+import DetailsUser from "../../components/Contents/DetailsUser";
+
+function Comments() {
+  return (
+    <>
+      <DetailsUser />
+    </>
+  );
+}
+export default Comments;

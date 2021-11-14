f5a6b17e729c5376e39d57634753f075b705a2f7
rizzie03 <destin.rizzie@gmail.com>
2021-11-14 22:31:46 +0700

    adding something

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignUp from "./pages/signup/SignUp";
 import SignIn from "./pages/signin/SignIn";
-import Comments from "./pages/comments/Comments"
-import CommentsUser from "./pages/comments/CommentsUser"
+import Comments from "./pages/comments/Comments";
+import CommentsUser from "./pages/comments/CommentsUser";
 
 import Headers from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -16,17 +16,16 @@ function App() {
     <Router>
       <Headers />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/signin" element={<SignIn />} />
-        <Route exact path="/comments" element={<Comments />} />
-        <Route exact path="/comments/user" element={<CommentsUser />} />
-        <Route exact path="/events" element={<Events />} />
-        <Route path="*" element={<NotFound />} />
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/signup' element={<SignUp />} />
+        <Route exact path='/signin' element={<SignIn />} />
+        <Route exact path='/comments' element={<Comments />} />
+        <Route exact path='/comments/user' element={<CommentsUser />} />
+        <Route exact path='/events' element={<Events />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </Router>
   );
 }
-
 export default App;
--- src/components/Contents/Details.jsx
+++ src/components/Contents/Details.jsx
@@ -1,38 +1,40 @@
 import React from "react";
 import { FiCalendar } from "react-icons/fi";
-import detailstyle from "./details.module.scss";
+import detailstyle from "./details.module.scss"
 
 export default function Details() {
   return (
     <>
-      <div className={detailstyle.Details}>
-        <div className={detailstyle.Title}>
-          <div className={detailstyle.Top}>
+      <div className={detailstyle.details}>
+        <div className={detailstyle.title}>
+          <div className={detailstyle.top}>
             <h1>ESL Game: English on Your Feet!</h1>
             <div className={detailstyle.dateCategory}>
               <FiCalendar className={detailstyle.icon} />
               <p>SUN, OCT 24 @ 1:15 AM ICT</p>
-              <p>Business</p>
-              <img
+              <button className={detailstyle.busbtn}>Business</button>
+              <img className={detailstyle.image}
                 src='https://s3-alpha-sig.figma.com/img/2522/c0e4/6dc673b5918bfe793295837ff96b650c?Expires=1637539200&Signature=dtd~fd11Yh1SzdJD7qbNLncWupLdQSE28YIjo4dHe1pl5OWEjbH-OkreL1LYLV5GoBReQSKkp~zGa7ymkB4N8sjPLH~iXc9FwJMxp7gggzDTO3SGIOkiLbSAJZhIBjpU5PeoLtM4-CloJFZr-rzsfBhbL~97mdRsbb0BhTJYVVZHfGbtwtdcmJkl6RudD7Yc8QY1UyEvVzI528gixlH0wjeYaFE2lczq8yPFGOt~P79P-mTCMrTZcrenGDO0JmGDiOi1AX6Fsz4x~LZcoyAhldbuAZnNpMvLmmlPuwee2MG8uG5VANaVXfQNv5sICBr~y0YIA4aeXIv1BsKIiZolaw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
                 alt='{image}'
               />
             </div>
           </div>
-          <div className={detailstyle.Content}>
-            <div className={detailstyle.Container}>
-              <h1>Details</h1>
+          <div className={detailstyle.content}>
+            <div className={detailstyle.container}>
+              <h1 className={detailstyle.detailsh1}>Details</h1>
+              <br />
               <p>Welcome to the Parlor! Let's play English on Your Feet!™</p>
               <br />
-              <p>GET FEEDBACK</p>
-              <p>GAIN CONFIDENCE</p>
-              <p>LAYER UP YOUR ENGLISH!™</p>
+              GET FEEDBACK
               <br />
-              <p>
+              GAIN CONFIDENCE
+              <br />
+              LAYER UP YOUR ENGLISH!™
+              <br />
+              <br />
                 Everyone will have a chance to speak to the "audience" and
                 receive feedback from the audience and our coaches. You don't
                 need to prepare anything--just prepare to have fun and try!
-              </p>
               <br />
               <p>Relax. Layer Up your English!™ </p>
               <br />
@@ -72,9 +74,9 @@ export default function Details() {
               </p>
               <br />
             </div>
-            <div className={detailstyle.sidePanel}>
-              <div className={detailstyle.Avatar}>
-                <img
+            <div className={detailstyle.sidepanel}>
+              <div className={detailstyle.avatarpic}>
+                <img className={detailstyle.avatarside}
                   src='https://s3-alpha-sig.figma.com/img/35b3/2335/854ac739a1a54b9219037b25b41868ec?Expires=1637539200&Signature=Oz3SVcR-o82BCEN6DCjkzJ6Mz7~zXAkIJhyPQEEhwxntQxeEA~QbFQnSOqJRmcJAibnBlroJFLbEJz3Llc3yOzV0q7d6st~RJUBBGtVqmS~EsFv414yK543KFbnIqMucAdzyG7jlud4pYPpBuvWMsJnX4ofkmljy47gjUt6wV6zFeMiybHCkVgbOkKyxuHkYZqCWQZrRmOlEcbvPDEjmgPp6YTEETvXcCkm9H9dyJOZxCyNWK8XXARkLWNx5LT67HmohFKsPYJ3CVvnmd4HHmH6AvVuz-Z2k~LT50KlGFW6CTnv~b-oA8sU0-AXK-NllAJynMQ1OycWPIWSnAE5DGg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
                   alt=''
                 />
@@ -82,9 +84,9 @@ export default function Details() {
               <p>Created by</p>
               <h2>Pratur Anahata Pratama</h2>
             </div>
-            <div className={detailstyle.commentSection}>
+            <div className={detailstyle.commentsection}>
               <h1>Comments</h1>
-              <div className={detailstyle.avatarSide}>
+              <div className={detailstyle.avatarside}>
                 <img
                   src='https://s3-alpha-sig.figma.com/img/6989/fa55/0b70f9e3dda2c46d3dbb67885145014e?Expires=1637539200&Signature=Mm9VNezDRtsO37Zg-lLvWn4K3td1WnkryOWxBei29AhDj2x3MgAchhAeGV6ik~-Qp7mWSISij-jcLWmphWBnlmosCFPtHpuZmViBQCp21Y-7pDSlbCPqZBYdz4s2hH8rxapxL2yjbpVbsCcqAfNyH6FzP3lWvG-E9bE2Gdvk-qgLE-DaR-3V3u3cUI2ZAYW4TqwFMW0gwOSyDEKoOgfBI7HA727lqZMSEcUt6HUiAjvkgB2GQd6W83D5YSD9TVRa0atZOv~JEJwGvIk6hWYWqAYrx6WIa1ZmPNEFlysi6Xe4VIX-QldNHVIEGidnall2Z-W3GE50kvrqFDQaps9mOQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
                   alt=''
@@ -94,7 +96,7 @@ export default function Details() {
               <p>4 days ago</p>
               <p>Hi, do you have a youtube channel that I can subscribe?</p>
               <img
-                className={detailstyle.avatarSide}
+                className={detailstyle.avatarside}
                 src='https://s3-alpha-sig.figma.com/img/0fd8/59ba/f2cc8b48344c3f8585fb7530dd3e3609?Expires=1637539200&Signature=GuDEYjSraivSbmpIn-UJu60JdHtQoIo4VMjLSlwBKDK2EqbaUMpCebGRtBg8tbOB3NfoNDQDv8mlyyHIr-gcO8OOb5jFlIkKI4oGpUcD2ojcFA9Wa7v-qnUhsmtNbl9sVOkpApIigTRwWpZzhUq9yiH13PlfERCN~eRFxMqzJFG07hdJYGTOC4rIJq2xRmuQ1ezUzJkJfFw1Nhk1LPY0zDqVe89FCJr~DoGWTJMVOOtwIMrTIqfMm~Y4Cg3BRiSmPdcRSBsZ5FzPiZBqUV~f4X~cpzPxM5mdDz0NWqhz-jtDZ-VV9PGb86uCNKu89kDe0vn7F-xJjpr1OqMHiXp9Pg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
                 alt=''
               />
@@ -104,7 +106,7 @@ export default function Details() {
                 Hi pratur! I like your event, I already sent you an email about
                 my upcoming event, could you hosted my event?
               </p>
-              <div className={detailstyle.avatarSide}>
+              <div className={detailstyle.avatarside}>
                 <img
                   src='https://s3-alpha-sig.figma.com/img/0bd1/4e23/71a5ccbc3d1830e042fb195a85046580?Expires=1637539200&Signature=P0kUBrf~ZyWBUyyCsEX65VzuBsve8Ua4jRbeCATTKYi3fc1TXzgzrgxwyXmqF74tQhmvc1EOBFaER8zQl8hXMWBgy0HaIWl-AvAHEddtxhdSVbu7Nyn~d2xJwqgws7-b-4r4VekhC0Cjd5gNnJWaLW~2I5HFcsKbgYl1wBg9qQnAkZr8SceJI3dm5nJ44nggwQ2t9eNLDNAAlB1WRryQirh3jFdQ8JQXo4beuPxR15-qjIuDHs5b0gNIQBpkGjX2Bh040OKA-la4LarEU0p7NNWij8tlzDiDBV9qfsDE3Aa-mTW1Yu4oCHg8E8gCOVQoosTMRgetDqiYPUINapmr2A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
                   alt=''
@@ -113,7 +115,7 @@ export default function Details() {
               <h2>Bambang Prakoso</h2>
               <p>20 Minutes ago</p>
               <p>Good Event!</p>
-              <div className={detailstyle.avatarSide}>
+              <div className={detailstyle.avatarside}>
                 <img
                   src='https://s3-alpha-sig.figma.com/img/2a84/2041/24f7143e149b588ffaa3f84662432c78?Expires=1637539200&Signature=GUV1g~OhHmIZY6fTIwGNCvkZtdl1WpjM-wrcnLtEGs22i9803JXA~9pTsKk9frVlJnGMY6mlzM~bkdmZP-dEJ6out3ktrx3MG2uyJO-ZAjvSZbtAWxQKaXPj4UUoJtJjJm-PXO4Zo7FlGGEEezcM~Pn3tY5IQclY~cejrciFlYTUzRjCQ4OFTxION9HRLhbEf9J-ZJAlaqHLuus60I2ex-Zx5BYcvGd48Sejqj~kJs2B35nQ8lVtqqY2vSgQbEIymT2E7Er-C~ORcx9qp44ZycLwF0f6TML8IyOV7cGg4SLqBjNIsMbZrQNhZ6JQot0jyZLEjdMmEXNU~egc~lJH3A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
                   alt=''

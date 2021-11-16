361955d5a978f7b0c5cf01de2b94c594ad854665
channylim24 <channylim24@gmail.com>
2021-11-16 04:56:29 +0000

    Home/channy

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import SignUp from "./pages/signup/SignUp";
-import SignIn from "./pages/signin/SignIn";
-import Posts from "./pages/posts/Posts";
-// import CommentsUser from "./pages/comments/CommentsUser";
-
-import Headers from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import Home from "./pages/home/Home";
-import Events from "./pages/events/Events";
-import NotFound from "./pages/notfound/NotFound";
+import createRoutes from "./routes/routes";
 
 function App() {
-  return (
-    <Router>
-      <Headers />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/signin" element={<SignIn />} />
-        <Route exact path="/posts" element={<Posts />} />
-        <Route exact path="/events" element={<Events />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+  const routes = createRoutes();
+
+  return routes;
 }
 
 export default App;

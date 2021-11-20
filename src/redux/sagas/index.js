7557693d161a35773ad66cf1cdf05e09a43c903e
rizzie03 <destin.rizzie@gmail.com>
2021-11-20 11:52:10 +0700

    adding something

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -3,11 +3,7 @@ import { watchLogin, watchRegister } from "./authSaga";
 
 
 function* rootSaga() {
-<<<<<<< HEAD
-  yield all([watchGetComments]);
-=======
-  yield all(["semua watchers", watchLogin(), watchRegister()]);
->>>>>>> 7cd427beda0d7e03c8e54b69cae4b8b213615870
+  yield all([watchLogin(), watchRegister()]);
 }
 
 export default rootSaga;

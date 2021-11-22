544673ba023fed3cb5dfd6ce1da26789143c8d0a
Destin Riz <88780207+rizzie03@users.noreply.github.com>
2021-11-22 10:43:35 +0700

    Add files via upload

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,27 +1,33 @@
 import CardStyle from "./card.module.scss";
 // import CardImage from "../../assets/img/cardImage.jpg";
 import { Link } from "react-router-dom";
+// import post from "../../../src/pages/posts/Posts"
+import { useState } from "react";
+export default Card;
 
 function Card({ data }) {
   return (
     <>
       <div className={CardStyle.card}>
-        <Link to="#" className={CardStyle.cardLink}>
-          <img
-            src={data.image}
-            alt={`${data.title}`}
-            className={CardStyle.cardImage}
-          />
+        <Link to={`/posts/${data.id}`} className={CardStyle.cardLink}>
+          <div className={CardStyle.cardImageContainer}>
+            <img
+              src={data.imageEvent}
+              alt={`${data.title}`}
+              className={CardStyle.cardImage}
+            />
+          </div>
           <div className={CardStyle.cardText}>
             <button className={CardStyle.cardTextChip}>{data.category}</button>
-            <p className={CardStyle.cardTextTime}>{data.time}</p>
+            <p className={CardStyle.cardTextTime}>{data.createdAt}</p>
             <p className={CardStyle.cardTextPostTitle}>{data.title}</p>
-            <p className={CardStyle.cardTextAuthor}>{data.author}</p>
+            <p
+              className={
+                CardStyle.cardTextAuthor
+              }>{`${data.firstName} ${data.lastName}`}</p>
           </div>
         </Link>
       </div>
     </>
   );
 }
-
-export default Card;

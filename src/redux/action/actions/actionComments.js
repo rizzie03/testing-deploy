c425ca2be15b8dc0db1d8ef9c0d8fb3bc63e328c
Destin Riz <destin.rizzie@gmail.com>
2021-11-23 00:09:55 +0000

    Details/tintin

--- src/redux/action/actions/actionComments.js
+++ src/redux/action/actions/actionComments.js
@@ -1,16 +1,28 @@
-import { COMMENT_BEGIN } from "../actionsTypes/typesComments";
-import { DETAILS_BEGIN } from "../actionsTypes/typesDetails";
+import { GET_COMMENTS_BEGIN, POST_COMMENTS_BEGIN } from "../actionsTypes/typesComments";
+import { GET_DETAILS_BEGIN, POST_DETAILS_BEGIN } from "../actionsTypes/typesDetails";
 
-export const CommentAction = (body) => {
+export const getCommentsAction = (body) => {
   return {
-    type: COMMENT_BEGIN,
+    type: GET_COMMENTS_BEGIN,
     body
   };
 };
 
-export const DetailsAction = (body) => {
+export const postCommentsAction = (body) => {
   return {
-    type: DETAILS_BEGIN,
+    type: POST_COMMENTS_BEGIN,
+    body
+  };
+};
+export const getDetailsAction = (body) => {
+  return {
+    type: GET_DETAILS_BEGIN,
+    body
+  };
+};
+export const postDetailsAction = (body) => {
+  return {
+    type: POST_DETAILS_BEGIN,
     body
   };
 };
